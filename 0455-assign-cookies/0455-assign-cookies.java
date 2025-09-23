d346504362268438eb8a0c6937696fd13212d36f
Enakshee Mondal <enakshee1712@gmail.com>
2025-09-23 11:26:44 +0530

    Time: 10 ms (79.21%), Space: 48.6 MB (60.18%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+    
+     int i=0;
+     int j=0;
+    Arrays.sort(g) ;
+    Arrays.sort(s);
+    while(i< g.length && j<s.length)
+    {
+        if(g[i]<=s[j])
+        {
+            i=i+1;
+        }
+        j=j+1;
+    }
+    return i;
+    }
+}
+
